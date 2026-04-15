0697670f0d788898e576e729877edb99129a1a2a
Jingqim <jingqim.chn@gmail.com>
2026-04-15 18:24:23 +1000

    build: Split build script into modules (build, build_globals and build_utils)

--- build/platforms/gowin/devices/GW1NR-9/C7I6/output/.artifacts/autogen_top_wrapper.sv
+++ build/platforms/gowin/devices/GW1NR-9/C7I6/output/.artifacts/autogen_top_wrapper.sv
@@ -35,7 +35,7 @@
 import util::*;
 
 module autogen_top_wrapper #(
-    parameter reg   [(8*VERSION_CHARS)-1:0] VERSION                 = "FW: 8951513-dirty|2026-04-15 16:49:59",
+    parameter reg   [(8*VERSION_CHARS)-1:0] VERSION                 = "FW: fd270ef-dirty|2026-04-15 18:20:44",
 
     parameter int                           CLK_FREQUENCY_MHZ       = 51,
     parameter int                           UART_BAUD               = 115200,
---
+++ windows/build_utils.ps1
@@ -0,0 +1,29 @@
+# ---- TEXT FORMATTING ----
+$boldf      = "`e[1m"
+$underlinef = "`e[4m"
+$normf      = "`e[0m"
+
+# ---- COPYRIGHT ----
+$copyright = @"
+    The source code contained herein is provided on an "as is" basis. Brisbane Silicon, Pty Ltd.
+    disclaims any and all warranties, whether express, implied, or statutory, including any implied
+    warranties of merchantability or of fitness for a particular purpose. In no event shall Brisbane
+    Silicon, Pty Ltd. be liable for any incidental, punitive, or consequential damages of any kind
+    whatsoever arising from the use of this source code.
+
+    This disclaimer of warranty extends to the user of this source code and user's customers,
+    employees, agents, transferees, successors and assigns.
+
+    This is not a grant of patent rights.
+"@
+
+# ---- UTILITY FUNCTIONS ----
+
+function Test-SupportedPlatform {
+    param([string]$PlatformName)
+    $platformPath = "$PlatformsDir\$PlatformName"
+    if (Test-Path $platformPath) {
+        return $true
+    }
+    return $false
+}
